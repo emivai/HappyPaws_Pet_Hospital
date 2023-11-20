525d3c320e2e798e76e9000a2b67d065e0b605f7
Emilija Vaisvalavičiūtė <emilija.vaisvalaviciute@ktu.edu>
2023-11-20 13:07:48 +0200

    Update StartupAuth.cs

--- HappyPaws.API/Capabilities/StartupAuth.cs
+++ HappyPaws.API/Capabilities/StartupAuth.cs
@@ -18,6 +18,8 @@ namespace HappyPaws.API.Capabilities
             })
             .AddJwtBearer(o =>
             {
+                o.TokenValidationParameters.ValidateLifetime = true;
+                o.TokenValidationParameters.ClockSkew = TimeSpan.Zero;
                 o.TokenValidationParameters.ValidAudience = configuration["JWT:ValidAudience"];
                 o.TokenValidationParameters.ValidIssuer = configuration["JWT:ValidIssuer"];
                 o.TokenValidationParameters.IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]));
